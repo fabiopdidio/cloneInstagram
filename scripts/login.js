d593917e9f34f93d0d7e8308339f5cf0c012145b
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-06-26 13:44:12 -0300

    commit
    atualiza

---
+++ scripts/login.js
@@ -0,0 +1,21 @@
+function clicarNoBotao() {
+  var email = document.getElementById("campo-email").value;
+  var senha = document.getElementById("campo-senha").value;
+
+  document.getElementById("campo-email").classList.remove("input-error");
+  document.getElementById("campo-senha").classList.remove("input-error");
+
+  if (email === "") {
+    document.getElementById("campo-email").classList.add("input-error");
+    document.getElementById("campo-email").focus();
+  } else if (senha === "") {
+    document.getElementById("campo-senha").classList.add("input-error");
+    document.getElementById("campo-email").focus();
+  } else {
+    document.getElementById("login-button").disabled = true;
+    document.getElementById("login-button").style.opacity = 0.5;
+    document.getElementById("login-button").innerText = "Logando ...";
+    // redireciona para outra página
+    window.location.href = "./home.html";
+  }
+}
